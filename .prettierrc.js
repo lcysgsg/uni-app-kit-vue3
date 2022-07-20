5114c897d407ff14b745774fa34cc08220fc90f7
LiChaoYang <249313573@qq.com>
2022-07-20 14:39:10 +0800

    feat: prettier

---
+++ .prettierrc.js
@@ -0,0 +1,17 @@
+'use strict';
+/** @format */
+module.exports = {
+  singleQuote: true,
+  trailingComma: 'all',
+  printWidth: 120,
+  proseWrap: 'never',
+  endOfLine: 'lf',
+  overrides: [
+    {
+      files: '.prettierrc',
+      options: {
+        parser: 'json',
+      },
+    },
+  ],
+};
