dbf3b03a102b72502b575dd28a51ac9ab890ef40
LiChaoYang <249313573@qq.com>
2022-07-21 14:33:04 +0800

    update: env and types

--- src/env.d.ts
+++ src/env.d.ts
@@ -6,3 +6,23 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
+
+declare const process: {
+  env: {
+    NODE_ENV: 'development' | 'production';
+    RUN_BY_HBUILDERX: boolean;
+    UNI_COMPILER_VERSION: string;
+    UNI_DEBUG: boolean;
+    UNI_PLATFORM: string;
+    UNI_STAT_DEBUG: boolean;
+  };
+};
+
+interface ImportMetaEnv {
+  // 更多环境变量...
+  readonly VITE_APP_SERVER_URL: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
