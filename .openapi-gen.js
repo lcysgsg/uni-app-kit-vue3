e8315c8365bfead8e60da1e7cd0709efa6cc1e5e
LiChaoYang <249313573@qq.com>
2023-10-12 15:26:40 +0800

    update: 更新 openapi 调用方式

--- .openapi-gen.js
+++ .openapi-gen.js
@@ -1,59 +1,44 @@
+const openAPI = require('openapi2typescript-yaml');
 const path = require('path');
-module.exports = [
+const config = [
   // https://github.com/lcysgsg/openapi2typescript/blob/main/src/index.ts#L20
   // 参考 @umijs/openapi type GenerateServiceProps
-  // export type GenerateServiceProps = {
-  //     requestLibPath?: string;
-  //     requestImportStatement?: string;
-  //     /**
-  //      * api 的前缀
-  //      */
-  //     apiPrefix?:
-  //         | string
-  //         | ((params: {
-  //             path: string;
-  //             method: string;
-  //             namespace: string;
-  //             functionName: string;
-  //             autoExclude?: boolean;
-  //         }) => string);
-  //     /**
-  //      * 生成的文件夹的路径
-  //      */
-  //     serversPath?: string;
-  //     /**
-  //      * openAPI 3.0 的地址
-  //      */
-  //     schemaPath?: string;
-  //     /**
-  //      * 项目名称
-  //      */
-  //     projectName?: string;
-  //     hook?: {
-  //         /** 自定义函数名称 */
-  //         customFunctionName?: (data: OperationObject) => string;
-  //         /** 自定义类名 */
-  //         customClassName?: (tagName: string) => string;
-  //     };
-  //     namespace?: string;
-  //     mockFolder?: string;
-  // };
-  // ===========================================================
-  // = e.g.
-  // ===========================================================
   // {
-  //   projectName: 'api',
-  //   schemaPath: 'http://192.168.1.253:8000/ananliangliang/tt-order-backend/-/raw/main/openapi/api/openapi.yml',
+  //   projectName: 'system',
+  //   schemaPath: 'http://192.168.1.110:8080/v2/api-docs?group=%E7%B3%BB%E7%BB%9F%E6%A8%A1%E5%9D%97',
   //   requestLibPath: "import { request } from '@/utils/request'",
   //   hook: {
   //     customFunctionName(OperationObject) {
   //       // 使用接口路径作函数名。
   //       // '/business_under/car-back/removeByIds'.replace(/(\/|-|_).{0,1}/g, (w) => w[1].toLocaleUpperCase())
   //       // => 'BusinessUnderCarBackRemoveByIds'
-  //       return OperationObject.path
-  //         .replace(/\/\{.*\}/g, '')
-  //         .replace(/(\/|-|_).{0,1}/g, (w) => w[1].toLocaleUpperCase());
+  //       // console.log(`[info]::OperationObject.path`, OperationObject.path);
+  //       if (OperationObject.path === '/') return OperationObject.path;
+  //       const nameWithPath = [];
+  //       if (Array.isArray(OperationObject.parameters)) {
+  //         for (let i = 0; i < OperationObject.parameters.length; i++) {
+  //           const param = OperationObject.parameters[i];
+  //           if (param.in === 'path') {
+  //             nameWithPath.push(param.name);
+  //           }
+  //         }
+  //       }
+  //       const nameWithPathStr = nameWithPath.map((e) => e.slice(0, 1).toLocaleUpperCase() + e.slice(1)).join('');
+  //       const nameWithMethodStr = OperationObject.method
+  //         ? OperationObject.method.slice(0, 1).toLocaleUpperCase() + OperationObject.method.slice(1).toLocaleLowerCase()
+  //         : '';
+  //       return (
+  //         OperationObject.path.replace(/\/\{.*\}/g, '').replace(/(\/|-|_).{0,1}/g, (w) => w[1].toLocaleUpperCase()) +
+  //         nameWithPathStr
+  //       );
   //     },
   //   },
   // },
 ];
+
+config.forEach((e) => {
+  openAPI.generateService({
+    serversPath: './src/services',
+    ...e,
+  });
+});
